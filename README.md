2593bfc5f7b5a318cd1c8fd595ace91719213411
Jared Grippe <jared@deadlyicon.com>
2017-04-04 14:48:45 -0700

    added fixture mode so you dont have to setup idm or game

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # Faces
 
 See all players by their avatar image. Names are hard.
+
+
+## Development
+
+### If you have `idm` and `game` running locally
+
+Your `.env` file should look something like this
+
+```sh
+PORT=3000
+IDM_BASE_URL=http://idm.learnersguild.dev
+GAME_BASE_URL=http://game.learnersguild.dev
+JWT_PUBLIC_KEY="-----BEGIN PUBLIC KEY-----\nMg3eJ…IDAQAB\n-----END PUBLIC KEY-----"
+```
+#### If you don't want to or cannot setup `idm` and `game`
+
+Your `.env` file should look like this
+
+```sh
+PORT=3000
+USE_FIXTURES=1
+```
+
+### Start the app
+
+```sh
+npm run start:dev
+```
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('./env')
 
+const fs = require('fs')
 const https = require('express-sslify').HTTPS
 const express = require('express')
 const cookieParser = require('cookie-parser')
@@ -17,29 +18,46 @@ if (process.env.NODE_ENV === 'production') {
 server.use(cookieParser())
 server.use(express.static(__dirname+'/public'))
 
-require('./authentication')(server)
+let getPlayers
+if (process.env.USE_FIXTURES) {
+  const players = require('./fixtures/'+process.env.NODE_ENV)
+  getPlayers = () => {
+    return Promise.resolve({
+      data: {
+        findUsers: players
+      }
+    })
+  }
+}else{
+
+  require('./authentication')(server)
+
+  getPlayers = (req) => {
+    return req.queryIdm(`
+      query {
+        findUsers {
+          name
+          handle
+          avatarUrl
+          email
+          active
+        }
+      }
+    `)
+  }
+}
+
 
 server.get('/', (req, res) => {
-  const query =
-  req.queryIdm(`
-    query {
-      findUsers {
-        name
-        handle
-        avatarUrl
-        email
-        active
-      }
-    }
-  `)
-  .then(results => {
-    const players = results.data.findUsers
-      .filter(player => player.active)
-    res.render('players', {players})
-  })
-  .catch(error => {
-    res.status(500).render('error', {error})
-  })
+  getPlayers(req)
+    .then(results => {
+      const players = results.data.findUsers
+        .filter(player => player.active)
+      res.render('players', {players})
+    })
+    .catch(error => {
+      res.status(500).render('error', {error})
+    })
 })
 
 server.get('/whoami', (req, res) => {
